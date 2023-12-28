7c415d4168e849d1f679a51c28ef0ebd70b0f498
Ayman Z. Behairy <abehairy@thiqah.sa>
2023-12-28 10:36:32 +0300

    implement formly layout for components

--- src/app/formaly/formaly.component.ts
+++ src/app/formaly/formaly.component.ts
@@ -33,89 +33,136 @@ export class FormalyComponent {
   //The field configurations for building the form.
   fields: FormlyFieldConfig[] = [
     {
-      key: 'email', //The key that relates to the model. This will link the field value to the model.
-      type: 'input',
-      // has lifecycle hooks like ones in angular (for customize your angular form)
-      // hooks: {
-      //   // you can access model + form + formControl , ... here
-      //   onInit: (field: FormlyFieldConfig) => {
-      //     if (field.props?.label) {
-      //       field.props.label = 'Email address';
-      //     }
-      //   },
-      // },
-      props: {
-        label: 'Email',
-        placeholder: 'Enter email',
-        required: true,
-        attributes: {
-          'data-cy': 'email',
+      fieldGroupClassName: 'row',
+      fieldGroup: [
+        {
+          className: 'col-6',
+          key: 'email', //The key that relates to the model. This will link the field value to the model.
+          type: 'input',
+          // has lifecycle hooks like ones in angular (for customize your angular form)
+          // hooks: {
+          //   // you can access model + form + formControl , ... here
+          //   onInit: (field: FormlyFieldConfig) => {
+          //     if (field.props?.label) {
+          //       field.props.label = 'Email address';
+          //     }
+          //   },
+          // },
+          props: {
+            label: 'Email',
+            placeholder: 'Enter email',
+            required: true,
+            attributes: {
+              'data-cy': 'email',
+            },
+          },
+          expressions: { 'props.label': this.translate.stream('Email') },
+          validators: {
+            email: {
+              expression: (c: any) =>
+                /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+                  c.value
+                ),
+              message: (error: any, field: FormlyFieldConfig) =>
+                `"${field.formControl?.value}" is not a valid email address`,
+            },
+          },
         },
-      },
-      expressions: { 'props.label': this.translate.stream('Email') },
-      validators: {
-        email: {
-          expression: (c: any) =>
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(c.value),
-          message: (error: any, field: FormlyFieldConfig) =>
-            `"${field.formControl?.value}" is not a valid email address`,
+        {
+          className: 'col-6',
+          key: 'age',
+          type: 'input',
+          props: {
+            type: 'number',
+            label: 'Age',
+            placeholder: 'Enter age',
+            required: true,
+            min: 12,
+            max: 40,
+          },
+          // overide predefine validators here (override global ones)
+          validation: {
+            messages: { max: 'please provid value less than 40' },
+          },
+          expressions: { 'props.label': this.translate.stream('Age') },
         },
-      },
+      ],
     },
     {
-      key: 'age',
-      type: 'input',
-      props: {
-        type: 'number',
-        label: 'Age',
-        placeholder: 'Enter age',
-        required: true,
-        min: 12,
-        max: 40,
-      },
-      // overide predefine validators here (override global ones)
-      validation: {
-        messages: { max: 'please provid value less than 40' },
-      },
-      expressions: { 'props.label': this.translate.stream('Age') },
-    },
-    {
-      key: 'countryId',
-      type: 'select',
-
-      props: {
-        label: 'select country',
-        options: this.dataService.getContries(),
-      },
-    },
-    {
-      key: 'cityId',
-      type: 'select',
-
-      props: {
-        label: 'select city',
-        options: [],
-      },
-      hooks: {
-        onInit: (field: FormlyFieldConfig) => {
-          if (field.props) {
-            const countryIdControl = field.form?.get('countryId');
-
-            if (countryIdControl) {
-              field.props.options = countryIdControl.valueChanges.pipe(
-                // to be not empty in initialization
-                startWith(this.model.countryId),
-                switchMap((countryId) => this.dataService.getCities(countryId))
-              );
-            }
-          }
+      fieldGroupClassName: 'row',
+      fieldGroup: [
+        {
+          key: 'countryId',
+          type: 'select',
+          className: 'col-6',
+          props: {
+            label: 'select country',
+            options: this.dataService.getContries(),
+          },
         },
-      },
-      //Expressions allow you to dynamically change field properties
-      expressions: {
-        'props.disabled': (field: FormlyFieldConfig) => !field.model.countryId, // using fn
-        'model.cityId': '!model.countryId ? null : model.cityId', // using template expression
-      },
+        {
+          key: 'cityId',
+          type: 'select',
+          className: 'col-6',
+          props: {
+            label: 'select city',
+            options: [],
+          },
+          hooks: {
+            onInit: (field: FormlyFieldConfig) => {
+              if (field.props) {
+                const countryIdControl = field.form?.get('countryId');
+
+                if (countryIdControl) {
+                  field.props.options = countryIdControl.valueChanges.pipe(
+                    // to be not empty in initialization
+                    startWith(this.model.countryId),
+                    switchMap((countryId) =>
+                      this.dataService.getCities(countryId)
+                    )
+                  );
+                }
+              }
+            },
+          },
+          //Expressions allow you to dynamically change field properties
+          expressions: {
+            'props.disabled': (field: FormlyFieldConfig) =>
+              !field.model.countryId, // using fn
+            'model.cityId': '!model.countryId ? null : model.cityId', // using template expression
+          },
+        },
+      ],
+    },
+    {
+      fieldGroupClassName: 'row',
+      fieldGroup: [
+        //Custom Input Component
+        {
+          key: 'firstName',
+          type: 'customInput',
+          className: 'col-6',
+          props: {
+            label: 'First Name',
+            placeholder: 'Enter your first name',
+            required: true,
+          },
+        },
+        {
+          key: 'ip',
+          type: 'input',
+          className: 'col-6',
+          props: {
+            label: 'Ip Adress',
+            placeholder: 'Enter your ip address',
+            required: true,
+          },
+          // custom validator
+          validators: {
+            validation: ['ip'],
+          },
+        },
+      ],
     },
     {
       key: 'agree',
@@ -126,41 +173,17 @@ export class FormalyComponent {
     },
     {
       key: 'comment',
-      type: 'input',
+      type: 'textarea',
       props: {
         label: 'coment',
         placeholder: 'Enter your comment',
+        rows: 3,
       },
       expressions: {
         hide: '!model.agree',
       },
     },
-    {
-      key: 'ip',
-      type: 'input',
-      props: {
-        label: 'Ip Adress',
-        placeholder: 'Enter your ip address',
-        required: true,
-      },
-      // custom validator
-      validators: {
-        validation: ['ip'],
-      },
-    },
-    //Custom Input Component
-    {
-      key: 'firstName',
-      type: 'customInput',
-      props: {
-        label: 'First Name',
-        placeholder: 'Enter your first name',
-        required: true,
-        // errorMessages: {
-        //   required: 'This field is required',
-        // },
-      },
-    },
+
     // hidden fields
     {
       key: 'id',
